a167a0f20dceae192c97ce876ccc1d0dc274aa10
Marian <markus8916@gmail.com>
2026-09-12 21:41:58 +0300

    Revert "Break test intentionally"
    This reverts commit 364a07f35da6cd4da98e976a5262d5a99a80d344.

--- Demo.Tests/CalculatorTests.cs
+++ Demo.Tests/CalculatorTests.cs
@@ -5,7 +5,7 @@ public class CalculatorTests
 {
     [Fact]
     public void Add_ReturnsSum() =>
-        Assert.Equal(6, new Calculator().Add(2, 3));
+        Assert.Equal(5, new Calculator().Add(2, 3));
 
     [Fact]
     public void Divide_ByZero_Throws() =>
